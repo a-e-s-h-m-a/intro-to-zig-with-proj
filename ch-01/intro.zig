31181af3024fc618f107bcfce942d24dbed49941
Aeshma <madhushanka2727@gmail.com>
2026-01-02 22:30:05 +0530

    scope

--- ch-01/intro.zig
+++ ch-01/intro.zig
@@ -4,10 +4,19 @@ pub fn main() void {
     // const ns = [4]u8{48, 24, 12, 6};
     // const sl = ns[1..3];
     // std.debug.print("{d}\n", .{sl.len});
-    const a = [_]u8{1,2,3};
-    const b = [_]u8{4,5};
-    const c = a ++ b;
-    std.debug.print("++ {any}\n", .{c});
-    const d = a ** 3;
-    std.debug.print("** {any}\n", .{d});
+    // const a = [_]u8{1,2,3};
+    // const b = [_]u8{4,5};
+    // const c = a ++ b;
+    // std.debug.print("++ {any}\n", .{c});
+    // const d = a ** 3;
+    // std.debug.print("** {any}\n", .{d});
+    // 
+    var y: i32 = 123;
+    const x = add_one: {
+        y += 1;
+        break :add_one y;
+    };
+    if (x == 124 and y == 124) {
+        std.debug.print("Hey!\n", .{});
+    }
 }
