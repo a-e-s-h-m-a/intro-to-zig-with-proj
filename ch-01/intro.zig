5dde4cc3a33bda9191feebebd359eb7ea89ee18b
Aeshma <madhushanka2727@gmail.com>
2026-01-02 22:09:03 +0530

    array concat

--- ch-01/intro.zig
+++ ch-01/intro.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
 
 pub fn main() void {
-    const ns = [4]u8{48, 24, 12, 6};
-    const sl = ns[1..3];
-    std.debug.print("{d}\n", .{sl.len});
+    // const ns = [4]u8{48, 24, 12, 6};
+    // const sl = ns[1..3];
+    // std.debug.print("{d}\n", .{sl.len});
+    const a = [_]u8{1,2,3};
+    const b = [_]u8{4,5};
+    const c = a ++ b;
+    std.debug.print("{any}", .{c});
 }
