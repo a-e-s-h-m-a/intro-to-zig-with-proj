0f84452d6d62b3b8d31afcebb5c3386a2f99cda9
Aeshma <madhushanka2727@gmail.com>
2026-01-02 21:58:53 +0530

    full slice short

--- ch-01/intro.zig
+++ ch-01/intro.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn main() void {
     const ns = [4]u8{48, 24, 12, 6};
-    const sl = ns[0..ns.len];
+    const sl = ns[0..];
     std.debug.print("{any}\n", .{sl});
 }
