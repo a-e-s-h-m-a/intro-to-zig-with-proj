c1abdb1ec4cc47a554ef0b11cb6e405062d6e0dd
Aeshma <madhushanka2727@gmail.com>
2026-01-02 22:06:44 +0530

    slice

--- ch-01/intro.zig
+++ ch-01/intro.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn main() void {
     const ns = [4]u8{48, 24, 12, 6};
-    const sl = ns[0..];
-    std.debug.print("{any}\n", .{sl});
+    const sl = ns[1..3];
+    std.debug.print("{d}\n", .{sl.len});
 }
