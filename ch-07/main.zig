75f8d3ff65b7cd57890e352f00d52f12ed89841a
Aeshma <madhushanka2727@gmail.com>
2026-01-21 22:13:10 +0530

    send response back to the client

--- ch-07/main.zig
+++ ch-07/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Request = @import("request.zig");
 const Server = @import("server.zig").Server;
+const Response = @import("response.zig");
 
 pub fn main() !void {
     var alloc = std.heap.GeneralPurposeAllocator(.{}){};
@@ -13,11 +14,17 @@ pub fn main() !void {
     var listening = try server.listen();
     const connection = try listening.accept(io);
     defer connection.close(io);
-    
+
     var request_buffer: [1000]u8 = undefined;
     @memset(request_buffer[0..], 0);
     try Request.read_request(io, connection, request_buffer[0..]);
-    
+
     const request = Request.parse_request(request_buffer[0..]);
-    std.debug.print("{any}\n", .{request});
+    if (request.method == .GET) {
+        if (std.mem.eql(u8, request.uri, "/")) {
+            try Response.send_200(connection, io);
+        } else {
+            try Response.send_404(connection, io);
+        }
+    }
 }
