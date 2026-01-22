5f700cafbead16128e6c72f5864d94547a6a5432
Aeshma <madhushanka2727@gmail.com>
2026-01-22 19:47:01 +0530

    Testing errors

--- ch-08/simple_sum.zig
+++ ch-08/simple_sum.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const expect = std.testing.expect;
+const expectError = std.testing.expectError;
 
 fn some_memory_leak(allocator: Allocator) !void {
     const buffer = try allocator.alloc(u32, 10);
@@ -8,9 +9,23 @@ fn some_memory_leak(allocator: Allocator) !void {
     // Return without freeing the allocated memory
 } 
 
-test "memory leak" {
-    const allocator = std.testing.allocator;
-    try some_memory_leak(allocator);
+// test "memory leak" {
+//     const allocator = std.testing.allocator;
+//     try some_memory_leak(allocator);
+// }
+
+fn alloc_error(allocator: Allocator) !void {
+    var ibuffer = try allocator.alloc(u8, 100);
+    defer allocator.free(ibuffer);
+    ibuffer[0] = 2;
+}
+
+test "testing error" {
+    var buffer: [10]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+    
+    try expectError(error.OutOfMemory, alloc_error(allocator));
 }
 
 // test "testing simple sum" {
