88fe67efac057db962518f65d735abc73ffb03de
Aeshma <madhushanka2727@gmail.com>
2026-01-22 19:51:43 +0530

    error expectations in zig

--- ch-08/simple_sum.zig
+++ ch-08/simple_sum.zig
@@ -20,16 +20,42 @@ fn alloc_error(allocator: Allocator) !void {
     ibuffer[0] = 2;
 }
 
-test "testing error" {
-    var buffer: [10]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    const allocator = fba.allocator();
+// test "testing error" {
+//     var buffer: [10]u8 = undefined;
+//     var fba = std.heap.FixedBufferAllocator.init(&buffer);
+//     const allocator = fba.allocator();
     
-    try expectError(error.OutOfMemory, alloc_error(allocator));
-}
+//     try expectError(error.OutOfMemory, alloc_error(allocator));
+// }
 
 // test "testing simple sum" {
 //     const a: u8 = 2;
 //     const b: u8 = 2;
 //     try expect((a + b) == 4);
 // }
+
+// -----------------
+// expectEqual()
+// test "values are equal?" {
+//     const v1 = 15;
+//     const v2 = 18;
+//     try std.testing.expectEqual(v1, v2);
+// }
+
+// expectEqualSlices
+test "arrays are equal?" {
+    const array1 = [3]u32{1, 2, 3};
+    const array2 = [3]u32{1, 2, 3};
+    try std.testing.expectEqualSlices(
+        u32, &array1, &array2
+    );
+}
+
+// expectEqualStrings
+test "strings are equal?" {
+    const str1 = "hello, world!";
+    const str2 = "Hello, world!";
+    try std.testing.expectEqualStrings(
+        str1, str2
+    );
+}
