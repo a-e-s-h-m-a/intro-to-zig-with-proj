3013aa331476cd18e39e07c3ef13eadeae5ddc3f
Aeshma <madhushanka2727@gmail.com>
2026-01-22 19:37:42 +0530

    testing memory leaks with std.testing.allocator

--- ch-08/simple_sum.zig
+++ ch-08/simple_sum.zig
@@ -1,8 +1,20 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const expect = std.testing.expect;
 
-test "testing simple sum" {
-    const a: u8 = 2;
-    const b: u8 = 2;
-    try expect((a + b) == 4);
+fn some_memory_leak(allocator: Allocator) !void {
+    const buffer = try allocator.alloc(u32, 10);
+    _ = buffer;
+    // Return without freeing the allocated memory
+} 
+
+test "memory leak" {
+    const allocator = std.testing.allocator;
+    try some_memory_leak(allocator);
 }
+
+// test "testing simple sum" {
+//     const a: u8 = 2;
+//     const b: u8 = 2;
+//     try expect((a + b) == 4);
+// }
