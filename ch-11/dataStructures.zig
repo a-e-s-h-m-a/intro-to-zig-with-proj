ad1edec2199c3ee55935513f8cee305bc7f9eaa2
Aeshma <madhushanka2727@gmail.com>
2026-01-28 13:15:37 +0530

    pop()

--- ch-11/dataStructures.zig
+++ ch-11/dataStructures.zig
@@ -3,14 +3,19 @@ const std = @import("std");
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    
+
     var buffer = try std.ArrayList(u8).initCapacity(allocator, 100);
     defer buffer.deinit(allocator);
-    
+
     try buffer.append(allocator, 'H');
     try buffer.append(allocator, 'e');
     try buffer.append(allocator, 'l');
     try buffer.append(allocator, 'l');
-    try buffer.append(allocator, '0');
-    try buffer.appendSlice(allocator, "World");
+    try buffer.append(allocator, 'o');
+    try buffer.appendSlice(allocator, "World!");
+
+    const exclamation_mark = buffer.pop();
+    _ = exclamation_mark;
+
+    std.debug.print("{s}\n", .{buffer.items});
 }
