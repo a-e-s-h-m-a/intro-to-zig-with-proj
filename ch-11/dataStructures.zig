fafc4318896f919f247f8d06a1e0ef26aaa13394
Aeshma <madhushanka2727@gmail.com>
2026-01-28 18:26:37 +0530

    orderedRemove

--- ch-11/dataStructures.zig
+++ ch-11/dataStructures.zig
@@ -7,15 +7,28 @@ pub fn main() !void {
     var buffer = try std.ArrayList(u8).initCapacity(allocator, 100);
     defer buffer.deinit(allocator);
 
-    try buffer.append(allocator, 'H');
-    try buffer.append(allocator, 'e');
-    try buffer.append(allocator, 'l');
-    try buffer.append(allocator, 'l');
-    try buffer.append(allocator, 'o');
-    try buffer.appendSlice(allocator, "World!");
+    // try buffer.append(allocator, 'H');
+    // try buffer.append(allocator, 'e');
+    // try buffer.append(allocator, 'l');
+    // try buffer.append(allocator, 'l');
+    // try buffer.append(allocator, 'o');
+    // try buffer.appendSlice(allocator, "World!");
 
-    const exclamation_mark = buffer.pop();
-    _ = exclamation_mark;
+    // const exclamation_mark = buffer.pop();
+    // _ = exclamation_mark;
 
-    std.debug.print("{s}\n", .{buffer.items});
+    // std.debug.print("{s}\n", .{buffer.items});
+    // 
+    for (0..10) |i| {
+        const index: u8 = @intCast(i);
+        try buffer.append(allocator, index);
+    }
+    
+    std.debug.print("{any}\n", .{buffer.items});
+    
+    _ = buffer.orderedRemove(3);
+    _ = buffer.orderedRemove(3);
+    
+    std.debug.print("{any}\n", .{buffer.items});
+    std.debug.print("{any}\n", .{buffer.items.len});
 }
