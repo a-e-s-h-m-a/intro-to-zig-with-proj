373f64c238a0a008dc8d01dae08a7f51684b6a0c
Aeshma <madhushanka2727@gmail.com>
2026-01-29 22:00:15 +0530

    iterating through hashtable

--- ch-11/hashTable.zig
+++ ch-11/hashTable.zig
@@ -12,12 +12,20 @@ pub fn main() !void {
     try hash_table.put(50050, 55);
     try hash_table.put(57709, 41);
     
-    std.debug.print("{d} : values are stored\n", .{hash_table.count()});
-    std.debug.print("Value at key: 50050 is: {d}\n", .{hash_table.get(50050).?});
+    // std.debug.print("{d} : values are stored\n", .{hash_table.count()});
+    // std.debug.print("Value at key: 50050 is: {d}\n", .{hash_table.get(50050).?});
     
-    if (hash_table.remove(57709)) {
-        std.debug.print("Value of the key 57709 is removed\n", .{});
+    // if (hash_table.remove(57709)) {
+    //     std.debug.print("Value of the key 57709 is removed\n", .{});
+    // }
+    
+    // std.debug.print("{d} : values are stored\n", .{hash_table.count()});
+    
+    // --------- iterating through hashtable
+    var it = hash_table.iterator();
+    
+    while (it.next()) |kv| {
+        std.debug.print("Key: {d} ", .{kv.key_ptr.*});
+        std.debug.print("Val: {d}\n", .{kv.value_ptr.*});
     }
-    
-    std.debug.print("{d} : values are stored\n", .{hash_table.count()});
 }
