dd767423a2c1fa44bed14f95204ba2284e7e9301
mmad <madhushankamalith@gmail.com>
2026-04-30 22:26:35 +0530

    argument to comptime parameter must be comptime-known

--- ch-12/main.zig
+++ ch-12/main.zig
@@ -4,8 +4,25 @@ fn twice(comptime num: u32) u32 {
     return num * 2;
 }
 
-pub fn main() !void {
-    
+pub fn main(init: std.process.Init) !void {
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.Io.File.stdout().writer(init.io, &stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
+    var stdin_buffer: [1024]u8 = undefined;
+    var stdin_reader = std.Io.File.stdin().reader(init.io, &stdin_buffer);
+    const stdin = &stdin_reader.interface;
+
+    var buffer: [5]u8 = .{ 0, 0, 0, 0, 0 };
+    _ = try stdout.write("Please type a 4-digit integer number:\n");
+    _ = try stdin.takeDelimiterExclusive('\n');
+
+    try stdout.print("Input: {s}", .{buffer});
+    const n: u32 = try std.fmt.parseInt(u32, buffer[0 .. buffer.len - 1], 10);
+    const twise_result = twice(n);
+
+    try stdout.print("Result: {d} \n", .{twise_result});
+    try stdout.flush();
 }
 
 test "twice" {
