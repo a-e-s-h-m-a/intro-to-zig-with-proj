df3af08a389b669e2208e9a09e56530fadc6d6df
mmad <madhushankamalith@gmail.com>
2026-05-03 20:45:24 +0530

    Add stdin example

--- ch-13/main.zig
+++ ch-13/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
     // ------------ read
-    
+
     const cwd = std.Io.Dir.cwd();
     const file = try cwd.openFile(init.io, "foo.txt", .{ .mode = .read_only });
     defer file.close(init.io);
@@ -24,4 +24,20 @@ pub fn main(init: std.process.Init) !void {
 
     try stdout.writeAll("This message is written into stdout.\n");
     try stdout.flush();
+
+    // ------------ stdin
+    var stdin_buffer2: [1024]u8 = undefined;
+    var stdout_buffer2: [1024]u8 = undefined;
+    var stdout_writer2 = std.Io.File.stdout().writer(init.io, &stdout_buffer2);
+    var stdin_reader2 = std.Io.File.stdin().reader(init.io, &stdin_buffer2);
+    const stdin2 = &stdin_reader2.interface;
+    const stdout2 = &stdout_writer2.interface;
+
+    try stdout2.writeAll("Type your name: ");
+    try stdout2.flush();
+
+    const name = try stdin2.takeDelimiterExclusive('\n');
+
+    try stdout2.print("Your name is: {s}\n", .{name});
+    try stdout2.flush();
 }
