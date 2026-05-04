d5dc9b79a5a8f02d60309e1045ce9d5e705bd823
mmad <madhushankamalith@gmail.com>
2026-05-04 22:56:17 +0530

    Add file reading functionality
    This commit modifies the `createFile.zig` program to include
    functionality for reading from the newly created file. It also adds a
    new file, `foo2.txt`, with some content to facilitate testing.

--- ch-13/createFile.zig
+++ ch-13/createFile.zig
@@ -1,9 +1,28 @@
 const std = @import("std");
 
+// pub fn main(init: std.process.Init) !void {
+//     const cwd = std.Io.Dir.cwd();
+//     const file = try cwd.createFile(init.io, "foo1.txt", .{});
+//     defer file.close(init.io);
+
+//     _ = try file.writePositionalAll(init.io, "writing this line to the file", 0);
+// }
+
 pub fn main(init: std.process.Init) !void {
     const cwd = std.Io.Dir.cwd();
-    const file = try cwd.createFile(init.io, "foo1.txt", .{});
+    const file = try cwd.createFile(init.io, "foo2.txt", .{ .read = true });
     defer file.close(init.io);
 
-    _ = try file.writePositionalAll(init.io, "writing this line to the file", 0);
+    _ = try file.writePositionalAll(init.io, "writing this line to the file 2", 0);
+
+    var buffer: [300]u8 = undefined;
+    @memset(buffer[0..], 0);
+
+    var read_buffer: [1024]u8 = undefined;
+    var fr = file.reader(init.io, &read_buffer);
+    var reader = &fr.interface;
+
+    _ = reader.readSliceAll(buffer[0..]) catch 0;
+    std.debug.print("What we read from the file is: {s}\n", .{buffer});
+    
 }
