0816990e0a7e93d1e89e1d87ed1a121b3e56c577
mmad <madhushankamalith@gmail.com>
2026-05-18 20:23:59 +0530

    feat: Add directory creation and deletion
    This commit introduces functionality to create single directories and
    nested directory paths. It also includes the deletion of a created
    directory.

--- ch-13/directory.zig
+++ ch-13/directory.zig
@@ -8,4 +8,11 @@ pub fn main(init: std.process.Init) !void {
     while (try it.next(init.io)) |entry| {
         std.debug.print("File name: {s}\n", .{entry.name});
     }
+
+    //create directories
+    try cwd.createDir(init.io, "testSingleDir", .default_dir);
+    try cwd.createDirPath(init.io, "testSingleDir/test1/test2/");
+
+    // Delete dir
+    try cwd.deleteDir(init.io, "testSingleDir");
 }
